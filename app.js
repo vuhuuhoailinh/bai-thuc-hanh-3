155832126079fe54fbc60cafb3e3783dfe723092
vuhuuhoailinh <57260268+vuhuuhoailinh@users.noreply.github.com>
2024-05-08 14:23:07 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const videoCardContainer = document.querySelector('.video-container');
 
-let api_key = "AIzaSyCPQe_MjhZTXKsCciyE2UzzPUS4ogE87r4";
+let api_key = "api key";
 let video_http = "https://www.googleapis.com/youtube/v3/videos?";
 let channel_http = "https://www.googleapis.com/youtube/v3/channels?";
 
